20eb18dfbf3c42a8abd4e9d8fd8895a840593550
Yeongtae <hwak1234@hanmail.net>
2018-12-13 18:06:04 +0900

    update synthesis scheme

--- synthesis.py
+++ synthesis.py
@@ -23,18 +23,55 @@ from train import load_model
 from text import text_to_sequence
 from mel2samp import MAX_WAV_VALUE
 
-def plot_data(data, figsize=(16, 4)):
+def plot_data(data, index, output_dir="", figsize=(16, 4)):
     fig, axes = plt.subplots(1, len(data), figsize=figsize)
     for i in range(len(data)):
         axes[i].imshow(data[i], aspect='auto', origin='bottom',
                         interpolation='none')
-    plt.savefig('test.png')
+    plt.savefig(os.path.join(output_dir, 'sentence_{}.png'.format(index)))
 
-def run(sigma, taco_cp_path = "", wg_cp_path ="", cleaner=['english_cleaners'], is_fp16=True):
+def generate_mels(taco2, sentences, cleaner, output_dir=""):
+    output_mels = []
+    for i, s in enumerate(sentences):
+        sequence = np.array(text_to_sequence(s, [cleaner]))[None, :]
+        sequence = torch.autograd.Variable(torch.from_numpy(sequence)).cuda().long()
+
+        stime = time.time()
+        _, mel_outputs_postnet, _, alignments = taco2.inference(sequence)
+        plot_data((mel_outputs_postnet.data.cpu().numpy()[0],
+                   alignments.data.cpu().numpy()[0].T), i, output_dir)
+        inf_time = time.time() - stime
+        print("{}th sentence, tacotron Infenrece time: {:.2f}s, len_mel: {}".format(i, inf_time, mel_outputs_postnet.size(2)))
+        output_mels.append(mel_outputs_postnet)
+
+    return output_mels
+
+def mels_to_wavs_WG(waveglow, mels, sigma, is_fp16, hparams, output_dir=""):
+    for i, mel in enumerate(mels):
+        mel = mel[-1, :, :]
+        mel = torch.autograd.Variable(mel.cuda())
+        mel = torch.unsqueeze(mel, 0)
+        mel = mel.half() if is_fp16 else mel
+        #mel = mel.data
+        stime = time.time()
+        with torch.no_grad():
+            audio = MAX_WAV_VALUE * waveglow.infer(mel, sigma=sigma)[0]
+        inf_time = time.time() - stime
+        audio = audio.data.cpu().numpy().astype('int16')
+        len_audio = float(len(audio)) / float(hparams.sampling_rate)
+        str = "{}th sentence, audio length: {:.2f} sec,  waveglow inf time: {:.2f}".format(i, len_audio, inf_time)
+        print(str)
+        write(os.path.join(output_dir, "sentence_{}.wav".format(i)), hparams.sampling_rate, audio)
+
+def run(sigma, sentence_path, taco_cp_path = "", wg_cp_path ="", cleaner='english_cleaners', output_dir='', is_fp16=True):
     hparams = create_hparams()
     hparams.sampling_rate = 22050
     # set 80 if u use korean_cleaners. set 149 if u use english_cleaners
-    hparams.n_symbols = 80 if cleaner[0] is 'korean_cleaners' else 149
+    hparams.n_symbols = 80 if cleaner == 'korean_cleaners' else 149
+
+    f = open(sentence_path,'r')
+    sentences = [x.strip() for x in f.readlines()]
+    f.close()
 
     model = load_model(hparams)
     model.load_state_dict(torch.load(taco_cp_path)['state_dict'])
@@ -42,40 +79,15 @@ def run(sigma, taco_cp_path = "", wg_cp_path ="", cleaner=['english_cleaners'], 
     waveglow = torch.load(wg_cp_path)['model']
     waveglow.remove_weightnorm()
     waveglow.cuda().eval()
+
     if is_fp16:
         waveglow.half()
         for k in waveglow.convinv:
             k.float()
 
-    text = "Scientists at the CERN laboratory say they have discovered a new particle."
-    start = time.perf_counter()
-    sequence = np.array(text_to_sequence(text, cleaner))[None, :]
-    sequence = torch.autograd.Variable(
-        torch.from_numpy(sequence)).cuda().long()
-    mel_outputs, mel_outputs_postnet, _, alignments = model.inference(sequence)
-    duration = time.perf_counter() - start
-    print("Tacotron2 inference time {:.2f}s/it".format(duration))
+    mel_outputs = generate_mels(model, sentences, cleaner, output_dir)
+    mels_to_wavs_WG(waveglow, mel_outputs, sigma, is_fp16, hparams, output_dir)
 
-    # save figure
-    plot_data((mel_outputs.data.cpu().numpy()[0],
-               mel_outputs_postnet.data.cpu().numpy()[0],
-               alignments.data.cpu().numpy()[0].T))
-
-    mel = mel_outputs_postnet[-1, :, :]
-    mel = torch.autograd.Variable(mel.cuda())
-    mel = torch.unsqueeze(mel, 0)
-    mel = mel.half() if is_fp16 else mel
-    #mel = mel.data
-    start = time.perf_counter()
-    with torch.no_grad():
-        audio = MAX_WAV_VALUE * waveglow.infer(mel, sigma=sigma)[0]
-        #audio = waveglow.infer(mel, sigma=sigma)[0]
-    duration = time.perf_counter() - start
-    print("Waveglow inference time {:.2f}s/it".format(duration))
-
-    audio = audio.data.cpu().numpy().astype('int16')
-    print(audio.max(), audio.min(), audio.shape, audio.dtype)
-    write('test.wav', hparams.sampling_rate, audio)
 
 if __name__ == "__main__":
     import argparse
@@ -85,15 +97,19 @@ if __name__ == "__main__":
 
     #sigma , taco_cp_path = "tacotron2/tacotron2_statedict.pt", wg_cp_path ="waveglow_old.pt", text = '', cleaner=['english_cleaners'], is_fp16=True
     parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--output_directory', type=str, default='',
+                        help='directory to save wave and fig')
     parser.add_argument('-t', "--taco_cp_path",
                         help='Path to tacotron decoder checkpoint with model', required=True)
     parser.add_argument('-w', '--wg_cp_path',
                         help='Path to waveglow decoder checkpoint with model', required=True)
     parser.add_argument('-c', "--cleaner")
-    parser.add_argument("-s", "--sigma", default=1.0, type=float)
+    parser.add_argument('-s','--sentence_path', type=str, default=None,
+                        required=True, help='sentence path')
+    parser.add_argument("--sigma", default=1.0, type=float)
     parser.add_argument("--is_fp16", action="store_true")
 
     args = parser.parse_args()
 
-    run(args.sigma, args.taco_cp_path, args.wg_cp_path, [args.cleaner], args.is_fp16)
+    run(args.sigma, args.sentence_path, args.taco_cp_path, args.wg_cp_path, args.cleaner, args.output_directory, args.is_fp16)
 
